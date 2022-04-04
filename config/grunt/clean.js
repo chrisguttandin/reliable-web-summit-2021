a796f71938a593f909fa3b70cc2d9ca38adfd898
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-04-05 00:16:18 +0200

    build(config): update build config

--- config/grunt/clean.js
+++ config/grunt/clean.js
@@ -1,7 +1,4 @@
 module.exports = {
-    'assets': ['build/reliable-web-summit-2021/**.ico', 'build/reliable-web-summit-2021/**.jpg', 'build/reliable-web-summit-2021/**.png'],
     'runtime': ['build/reliable-web-summit-2021/runtime*.js'],
-    'scripts': ['build/reliable-web-summit-2021/!(ngsw-worker).js'],
-    'source-maps': ['build/reliable-web-summit-2021/**.map'],
-    'styles': ['build/reliable-web-summit-2021/**.css']
+    'source-maps': ['build/reliable-web-summit-2021/**.map']
 };
--- config/grunt/copy.js
+++ config/grunt/copy.js
@@ -8,35 +8,5 @@ module.exports = {
                 src: ['404.html']
             }
         ]
-    },
-    assets: {
-        files: [
-            {
-                cwd: 'build/reliable-web-summit-2021/',
-                dest: 'build/reliable-web-summit-2021/assets/',
-                expand: true,
-                src: ['*.ico', '*.jpg', '*.png']
-            }
-        ]
-    },
-    scripts: {
-        files: [
-            {
-                cwd: 'build/reliable-web-summit-2021/',
-                dest: 'build/reliable-web-summit-2021/scripts/',
-                expand: true,
-                src: ['**/!(ngsw-worker).js']
-            }
-        ]
-    },
-    styles: {
-        files: [
-            {
-                cwd: 'build/reliable-web-summit-2021/',
-                dest: 'build/reliable-web-summit-2021/styles/',
-                expand: true,
-                src: ['**/*.css']
-            }
-        ]
     }
 };
