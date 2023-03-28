717b5712d3365a4f22191f569362d14292add964
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-03-28 20:17:10 +0200

    build(config): update Playwright config

--- config/playwright/config.ts
+++ config/playwright/config.ts
@@ -39,7 +39,7 @@ const config: PlaywrightTestConfig = {
     retries: env.CI ? 2 : 0,
     snapshotDir: '../../test/screenshots',
     testDir: '../../test',
-    testMatch: /(e2e|regression)\/.*\.ts/,
+    testMatch: /(e2e|regression)\/(?!.*\.po\.ts$).*\.ts$/,
     use: {
         baseURL: env.IS_SMOKE_TEST ? 'https://chrisguttandin.github.io/reliable-web-summit-2021/' : 'http://localhost:6622',
         trace: 'on-first-retry'
